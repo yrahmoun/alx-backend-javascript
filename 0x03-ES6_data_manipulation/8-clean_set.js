1d9975b815acef281d19f81a41a48e32ac555864
Rahmouni Yasser <rahmouniyasser77@gmail.com>
2024-06-03 13:13:58 +0000

    task 8

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,11 @@
+export default function cleanSet(set, startString) {
+  const strings = [];
+
+  if (startString === '' || typeof startString !== 'string') return '';
+  set.forEach((s) => {
+    if (typeof s === 'string' && s.startsWith(startString)) {
+      strings.push(s.slice(startString.length));
+    }
+  });
+  return strings.join('-');
+};
